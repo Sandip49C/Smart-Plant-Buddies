bf31d277db0c495eb460e4fcaf32fb0fac63149e
raymonkc10 <203017344+raymonkc10@users.noreply.github.com>
2025-12-05 11:21:23 +0545

    feat: register services and initialize database

---
+++ MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using SmartPlantBuddy.Services;
+
+namespace SmartPlantBuddy;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        builder.Services.AddSingleton<DatabaseHelper>();
+        builder.Services.AddSingleton<DatabaseRepository>();
+        builder.Services.AddSingleton<SecurePinService>();
+        builder.Services.AddSingleton<LocationService>();
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
